27ec01fe5efa1fa3f95b057ecc21e40453a4ca6a
Majid Mido <majidmido03@gmail.com>
2022-01-19 21:33:38 +0100

    font fixes

--- components/Post.jsx
+++ components/Post.jsx
@@ -14,7 +14,7 @@ const Post = ({ postData }) => {
         {postData.title}
       </h1>
 
-      <p className="font-Montserrat opacity-80 whitespace-pre-line tracking-wide text-2xl">
+      <p className="font-Oswald font-light opacity-80 whitespace-pre-line tracking-wide text-2xl">
         {postData.fullPostText}
       </p>
       
